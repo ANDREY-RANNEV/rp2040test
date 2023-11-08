692bb672be45027421efe8ea417f84f530d0c711
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2023-11-08 13:53:16 +0400

    cvc

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <stdio.h>
 
 // put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main()
+{
+  Serial1.begin(115200);
+  Serial.begin(115200);
+  SerialUSB.begin(115200);
+  pinMode(25, DEVICE_PORT_OUT);
+  digitalWrite(25, 1);
+  delay(500);
+  digitalWrite(25, 0);
+  delay(500);
+  Serial1.print("Hello!!\nFirst test 1\n");
+  Serial.print("Hello!!\nFirst test 0\n");
+  SerialUSB.print("Hello!!\nFirst test u\n");
+  while (true)
+  {
+    digitalWrite(25, 1);
+    delay(500);
+    digitalWrite(25, 0);
+    delay(500);
+  }
+  return 0;
 }
